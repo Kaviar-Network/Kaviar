4d1982dcef232b049707d3456b5dc41dec1ca3ca
kunpengren <675523481@qq.com>
2023-09-10 15:00:10 +0800

    draft of cross-chain contract

---
+++ contracts/Sender.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity ^0.7.3;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import { AxelarExecutable } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutable.sol';
+import { IAxelarGateway } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol';
+import { IAxelarGasService } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol';
+
+abstract contract Sender is AxelarExecutable, ReentrancyGuard {
+    uint256 public immutable denomination;
+    
+
+    mapping(bytes32 => bool) public nullifierHashes;
+
+    event Deposit(
+        bytes32 indexed commitment,
+        uint32 leafIndex,
+        uint256 timestamp
+    );
+   
+    /**
+    @param _verifier the address of SNARK verifier for this contract
+    @param _denomination transfer amount for each deposit
+    @param _merkleTreeHeight the height of deposits' Merkle Tree
+    */
+     constructor(address gateway_, address gasReceiver_) AxelarExecutable(gateway_) {
+        gasService = IAxelarGasService(gasReceiver_);
+    }
+
+    /**
+    @dev Deposit funds into the contract. The caller must send (for ETH) or approve (for ERC20) value equal to or `denomination` of this instance.
+    @param _commitment the note commitment, which is PedersenHash(nullifier + secret)
+  */
+    function deposit(bytes32 _commitment, string calldata destinationChain,
+        string calldata destinationAddress) external payable nonReentrant {
+        // call gateway contract to send commitment
+        // uint32 insertedIndex = _insert(_commitment);
+       require(msg.value > 0, 'Gas payment is required');
+
+        bytes memory payload = abi.encode(_commitment);
+        gasService.payNativeGasForContractCall{ value: msg.value }(
+            address(this),
+            destinationChain,
+            destinationAddress,
+            payload,
+            msg.sender
+        );
+        gateway.callContract(destinationChain, destinationAddress, payload);
+    }
+
+    /** @dev this function is defined in a child contract */
+    function _processDeposit() internal virtual;
+
+}
