807d7b92a923ecc16125cf61d99ee24917740abe
kunpengren <675523481@qq.com>
2023-09-12 01:13:28 +0800

    update deploy tornado

--- const.ts
+++ const.ts
@@ -1,8 +1,10 @@
 export const verifier = "0x065C6B312e3aa2b1824F2791AfD85f0D0f07f6a7";
 export const poseidonAddr = "0x70E21B9FF22ED107fBE869fAe2Dfd5Ce05dE6fe5";
-export const tornado = "0x88Dc222180a2e5c6C8aEca044Bb186B6557Bd765";
+export const tornado = "0xF9adAe57F174E3cB32F2AF07a69BC9e7b20Bf303";
 export const sender = "0xB741bc12b42E817B9FFc22b4AF4b2f8982071Bc1";
 export const receiver = "0x83B7e46230ab279952Ad210c414d554255Ea95a2";
+export const tornado_verifier = "0x0dc773Ab1dbbDB88393FA6a2c7366058ff9b164c"
+export const tornado_poseidonAddr = "0x85702e0dAc93a853D2893A4916EDFa5EA28784fA"
 
 export const goerliNet = {
     url: "https://eth-goerli.g.alchemy.com/v2/wQZhZrGooqQlJMi44xtV3WQEAlhY8Ycz",
