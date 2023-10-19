ef56ec22da0106dac9bb55507c6b62238a8781f7
reymom <ramon_gs95@hotmail.com>
2023-10-19 16:25:20 +0200

    add polygon consts and change deploy to polygon

--- const.ts
+++ const.ts
@@ -3,49 +3,58 @@ export const poseidonAddr = "0x70E21B9FF22ED107fBE869fAe2Dfd5Ce05dE6fe5";
 export const tornado = "";
 export const sender = "0x6971cf4d1A6A4D1B2d53Eb5793E07A7FB01B3bD9";
 export const receiver = "0x83B7e46230ab279952Ad210c414d554255Ea95a2";
-export const tornado_verifier = "0x0dc773Ab1dbbDB88393FA6a2c7366058ff9b164c"
-export const tornado_poseidonAddr = "0x85702e0dAc93a853D2893A4916EDFa5EA28784fA"
+export const tornado_verifier = "0x0dc773Ab1dbbDB88393FA6a2c7366058ff9b164c";
+export const tornado_poseidonAddr =
+  "0x85702e0dAc93a853D2893A4916EDFa5EA28784fA";
 export const verifierLinea = "0x86ECE5cfBeCeC21054fAC970141537aa57BE177d";
 export const poseidonAddrLinea = "0x52EE8690474938F6fEeB75EAb252f805873c11E1";
 export const receiverLinea = "0x550C6A96623a310eC1c446c27abE525d30c7f780";
-export const poseidonAddrMantle = "0x5bc78e38b0A6bfeDf6fF93617b14C27e9D6dF4d5"
-export const verifierMantle = "0xbd83119F22A213861E783235ad17db8d71C45268"
-export const receiverMantle = "0x06f1527F4B3B302C5eEC867ac88cd64f5a4EB7Da"
-export const poseidonAddrScroll = "0x9d77bbf021c153a91116E3088ec92c606dB23FAc"
-export const verifierScroll = "0x28ac354197B3bA5f626cCb5fb2573D430855d9Fb"
+export const poseidonAddrMantle = "0x5bc78e38b0A6bfeDf6fF93617b14C27e9D6dF4d5";
+export const verifierMantle = "0xbd83119F22A213861E783235ad17db8d71C45268";
+export const receiverMantle = "0x06f1527F4B3B302C5eEC867ac88cd64f5a4EB7Da";
+export const poseidonAddrScroll = "0x9d77bbf021c153a91116E3088ec92c606dB23FAc";
+export const verifierScroll = "0x28ac354197B3bA5f626cCb5fb2573D430855d9Fb";
+export const senderPolygon = "0x632b39E5Fe4EAAFDF21601b2Bc206ca0f602C85A";
 
 export const goerliNet = {
-    url: "https://eth-goerli.g.alchemy.com/v2/wQZhZrGooqQlJMi44xtV3WQEAlhY8Ycz",
-    chainId: 5,
-    gateway:"0xe432150cce91c13a887f7D836923d5597adD8E31",
-    gasservice:"0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6",
-    name: "ethereum-2"
-}
+  url: "https://eth-goerli.g.alchemy.com/v2/wQZhZrGooqQlJMi44xtV3WQEAlhY8Ycz",
+  chainId: 5,
+  gateway: "0xe432150cce91c13a887f7D836923d5597adD8E31",
+  gasservice: "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6",
+  name: "ethereum-2",
+};
 export const bscNet = {
-    url: "https://bsc-testnet.publicnode.com",
-    chainId: 97,
-    gateway:"0x4D147dCb984e6affEEC47e44293DA442580A3Ec0",
-    gasservice:"0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6",
-    name: "binance"
-}
+  url: "https://bsc-testnet.publicnode.com",
+  chainId: 97,
+  gateway: "0x4D147dCb984e6affEEC47e44293DA442580A3Ec0",
+  gasservice: "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6",
+  name: "binance",
+};
 export const mantleNet = {
-    url:"https://rpc.testnet.mantle.xyz",
-    chainId: 5001,
-    gateway:"0xe432150cce91c13a887f7D836923d5597adD8E31",
-    gasservice:"0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6",
-    name: "mantle"
-}
+  url: "https://rpc.testnet.mantle.xyz",
+  chainId: 5001,
+  gateway: "0xe432150cce91c13a887f7D836923d5597adD8E31",
+  gasservice: "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6",
+  name: "mantle",
+};
 export const lineaNet = {
-    url:"https://rpc.goerli.linea.build",
-    chainId: 59140,
-    gateway: "0xe432150cce91c13a887f7D836923d5597adD8E31",
-    gasservice: "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6",
-    name: "linea"
-}
+  url: "https://rpc.goerli.linea.build",
+  chainId: 59140,
+  gateway: "0xe432150cce91c13a887f7D836923d5597adD8E31",
+  gasservice: "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6",
+  name: "linea",
+};
 export const scrollNet = {
-    url:"https://sepolia-rpc.scroll.io",
-    chainId: 534351,
-    gateway: "0xe432150cce91c13a887f7D836923d5597adD8E31",
-    gasservice: "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6",
-    name: "scrollSepolia"
-}
+  url: "https://sepolia-rpc.scroll.io",
+  chainId: 534351,
+  gateway: "0xe432150cce91c13a887f7D836923d5597adD8E31",
+  gasservice: "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6",
+  name: "scrollSepolia",
+};
+export const polygonNet = {
+  url: "https://rpc-mumbai.maticvigil.com",
+  chainId: 80001,
+  gateway: "0xe432150cce91c13a887f7D836923d5597adD8E31",
+  gasservice: "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6",
+  name: "polygon",
+};
--- scripts/deploy_Sender.ts
+++ scripts/deploy_Sender.ts
@@ -1,39 +1,36 @@
 import * as dotenv from "dotenv";
 import { ethers } from "ethers";
-import{ Sender__factory} from  "../types";
-import { bscNet, poseidonAddr, receiver } from "../const";
+import { Sender__factory } from "../types";
+import { polygonNet } from "../const";
 
 dotenv.config();
 async function main() {
-   
-    const wallet = new ethers.Wallet(process.env.userOldSigner ?? "")
-    const provider = new ethers.providers.StaticJsonRpcProvider(
-      bscNet.url,
-      bscNet.chainId
-    );
+  const wallet = new ethers.Wallet(process.env.polygonSigner ?? "");
+  const provider = new ethers.providers.StaticJsonRpcProvider(
+    polygonNet.url,
+    polygonNet.chainId
+  );
   //  const provider = new ethers.providers.JsonRpcProvider("https://data-seed-prebsc-1-s1.binance.org:8545", {name: "bsc", chainId: 97})
-    const signer = wallet.connect(provider);
-    // const balanceBN = await signer.getBalance();
-    // const balance = Number(ethers.utils.formatEther(balanceBN));
-    // console.log(`Wallet balance ${balance}`);
+  const signer = wallet.connect(provider);
+  // const balanceBN = await signer.getBalance();
+  // const balance = Number(ethers.utils.formatEther(balanceBN));
+  // console.log(`Wallet balance ${balance}`);
 
-    const ETH_AMOUNT = ethers.utils.parseEther("0.001");
-    const AXELAR_GAS = ethers.utils.parseEther("0.005");
-    const TOTAL_VALUE = ethers.utils.parseEther("0.006");
-    
-    
-    const verifier = await new Sender__factory(signer).deploy(
-        bscNet.gateway,
-        bscNet.gasservice,
-        ETH_AMOUNT,
-        AXELAR_GAS
-      );
-    await (verifier).deployed();
-    console.log(`Sender contract deployed to ${verifier.address}`);
+  const ETH_AMOUNT = ethers.utils.parseEther("0.001");
+  const AXELAR_GAS = ethers.utils.parseEther("0.005");
+  const TOTAL_VALUE = ethers.utils.parseEther("0.006");
 
+  const verifier = await new Sender__factory(signer).deploy(
+    polygonNet.gateway,
+    polygonNet.gasservice,
+    ETH_AMOUNT,
+    AXELAR_GAS
+  );
+  await verifier.deployed();
+  console.log(`Sender contract deployed to ${verifier.address}`);
 }
 
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-  })
+  console.error(error);
+  process.exitCode = 1;
+});
