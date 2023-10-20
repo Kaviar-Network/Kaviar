5d817b7ea29686d51dd23e2c59199ac2258eac9e
reymom <ramon_gs95@hotmail.com>
2023-10-20 15:39:30 +0200

    add polygon zqEVM to nets

--- frontend-app/src/wagmi.ts
+++ frontend-app/src/wagmi.ts
@@ -6,18 +6,33 @@ import {
   mantleTestnet,
   scrollSepolia,
   bscTestnet,
+  polygonZkEvmTestnet,
 } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 
 const walletConnectProjectId = "31b7a6907dcc1be39c4d4ca7e4ed20b1";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [polygonMumbai, goerli, mantleTestnet, scrollSepolia, bscTestnet],
+  [
+    polygonMumbai,
+    goerli,
+    mantleTestnet,
+    scrollSepolia,
+    bscTestnet,
+    polygonZkEvmTestnet,
+  ],
   [publicProvider()]
 );
 
 const { chains: rainbowChains } = configureChains(
-  [polygonMumbai, goerli, mantleTestnet, scrollSepolia, bscTestnet],
+  [
+    polygonMumbai,
+    goerli,
+    mantleTestnet,
+    scrollSepolia,
+    bscTestnet,
+    polygonZkEvmTestnet,
+  ],
   [publicProvider()]
 );
 
